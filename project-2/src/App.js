a038da03fe664cf529e68ab81a27a240237a7253
Ray Zana <rayzana280@gmail.com>
2023-06-18 13:43:25 -0400

    adding react route with react hooks

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -8,9 +8,17 @@ import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Home />
-      <Selection />
-      <Sell />
+      <Switch>
+        <Route path="/selection">
+          <Selection />
+        </Route>
+        <Route path="/sell">
+          <Sell />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
