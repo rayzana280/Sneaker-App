be1eb3decd34a08b68a0bc7dd6ac58551bcd4425
Ray Zana <rayzana280@gmail.com>
2023-06-19 12:07:07 -0400

    importing NavBar to app now Clinet can navigate to either of three page

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Home from "./components/Home";
 import Selection from "./components/Selection";
 import Sell from "./components/Sell";
+import NavBar from "./components/NavBar"
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Switch>
         <Route path="/selection">
           <Selection />
