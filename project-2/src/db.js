b989f78190a1d5365443917c6de95231f64447af
Ray Zana <rayzana280@gmail.com>
2023-06-20 23:16:50 -0400

    adding array of object called shoe

---
+++ project-2/src/db.js
@@ -0,0 +1,12 @@
+const shoe = [
+    {
+        name : "Black Cat",
+        color : "black",
+        image : "https://www.nicekicks.com/files/2019/11/leadOliviaKimxAirJordan4-11-6-robmata-1-1200x640.jpg"
+    },
+    {
+        name : "Bred Toe",
+        color : "red, black",
+        image : "https://m.media-amazon.com/images/I/71Lvsa8HHrL._AC_UY1000_.jpg"
+    }
+]
