79b2d3d6bc55dad78ad867076ce5beb5a78078ce
Ray Zana <rayzana280@gmail.com>
2023-06-21 00:41:49 -0400

    getting prop from App component and using map to display shoeData's shoeCard for each shoe, also importing shoeCard component

--- project-2/src/components/Selection.js
+++ project-2/src/components/Selection.js
@@ -1,8 +1,15 @@
 import React from "react";
+import ShoeCard from "./ShoeCard" ;
 
-function Selection (){
+function Selection ({shoeData}){
+    //console.log(shoeData)
     return(
-        <div>sneaker selection</div>
+        <div>
+            <p>GET THE SNEAKERS FOR YOU</p>
+            {shoeData.map((item)=>{
+                return <ShoeCard key={item.name}/>
+            })}
+        </div>
     )
 };
 
