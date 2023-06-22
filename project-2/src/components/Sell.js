a197b1745debe8d9a74800d83d6243f9585e2034
Ray Zana <rayzana280@gmail.com>
2023-06-22 00:21:37 -0400

    adding state hook and creating a variable with state to create a value for the inout field

--- project-2/src/components/Sell.js
+++ project-2/src/components/Sell.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Sell(){
+
+    const [shoeName, stateShoeName] = useState("")
+    const [colorWay, stateColorWay] = useState("")
+    const [shoePrice, stateShoePrice] = useState("")
+    const [shoeImage, stateShoeImage] = useState("")
+    const [sellerEmail, stateSellerEmail] = useState("")
+
     return(
         <form>
             <h1>Sell Your Sneakers And Get Your Money!</h1>
             <label>Name of Shoe</label>
-            <input type="text"/>
+            <input type="text" value={shoeName}/>
             <label>ColorWay of Shoe</label>
-            <input type="text"/>
+            <input type="text" value={colorWay}/>
             <label>Price</label>
-            <input type="text"/>
+            <input type="text" value={shoePrice}/>
             <label>Image of Shoe</label>
-            <input type="text"/>
+            <input type="text" value={shoeImage}/>
             <label>Email</label>
-            <input type="text"/>
+            <input type="text" value={sellerEmail}/>
             <button type="submit">Submit</button>
         </form>
     )
