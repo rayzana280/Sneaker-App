30745862a022ade1753c7f71d8eb2073851c3814
Ray Zana <rayzana280@gmail.com>
2023-06-21 23:58:14 -0400

    setting up a form with input got name of shoe, colorway, price, image and email for sell component

--- project-2/src/components/Sell.js
+++ project-2/src/components/Sell.js
@@ -2,7 +2,20 @@ import React from "react";
 
 function Sell(){
     return(
-        <div>Sell sneakers</div>
+        <form>
+            <h1>Sell Your Sneakers And Get Your Money!</h1>
+            <label>Name of Shoe</label>
+            <input type="text"/>
+            <label>ColorWay of Shoe</label>
+            <input type="text"/>
+            <label>Price</label>
+            <input type="text"/>
+            <label>Image of Shoe</label>
+            <input type="text"/>
+            <label>Email</label>
+            <input type="text"/>
+            <button type="submit">Submit</button>
+        </form>
     )
 };
 
