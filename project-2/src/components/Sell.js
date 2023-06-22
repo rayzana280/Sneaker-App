f1b0645c08de053c01866f05ac8de9dd573ee763
Ray Zana <rayzana280@gmail.com>
2023-06-22 00:45:55 -0400

    using state to turn thi form into a controlled form and to get back the input value

--- project-2/src/components/Sell.js
+++ project-2/src/components/Sell.js
@@ -8,19 +8,31 @@ function Sell(){
     const [shoeImage, stateShoeImage] = useState("")
     const [sellerEmail, stateSellerEmail] = useState("")
 
+    function handleSubmit(e){
+        e.preventDefault();
+        const formData = {
+            name: shoeName,
+            color: colorWay,
+            price: shoePrice,
+            seller: sellerEmail,
+            image: shoeImage
+        }
+        console.log(formData)
+    }
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Sell Your Sneakers And Get Your Money!</h1>
             <label>Name of Shoe</label>
-            <input type="text" value={shoeName}/>
+            <input type="text" value={shoeName} onChange={(e)=> stateShoeName(e.target.value)}/>
             <label>ColorWay of Shoe</label>
-            <input type="text" value={colorWay}/>
+            <input type="text" value={colorWay} onChange={(e)=> stateColorWay(e.target.value)}/>
             <label>Price</label>
-            <input type="text" value={shoePrice}/>
+            <input type="text" value={shoePrice} onChange={(e)=> stateShoePrice(e.target.value)}/>
             <label>Image of Shoe</label>
-            <input type="text" value={shoeImage}/>
+            <input type="text" value={shoeImage} onChange={(e)=> stateShoeImage(e.target.value)}/>
             <label>Email</label>
-            <input type="text" value={sellerEmail}/>
+            <input type="text" value={sellerEmail} onChange={(e)=> stateSellerEmail(e.target.value)}/>
             <button type="submit">Submit</button>
         </form>
     )
