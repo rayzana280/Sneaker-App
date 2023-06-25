96aec061eb86c44d057f901a44cec4f891ce0da3
Ray Zana <rayzana280@gmail.com>
2023-06-25 12:18:01 -0400

    adding css to navLink

--- project-2/src/components/NavBar.js
+++ project-2/src/components/NavBar.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
 
+const linkStyles = {
+    display: "inline-block",
+    width: "78px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "darkgreen",
+    textDecoration: "none",
+    color: "white",
+  };
+
 function NavBar(){
     return (
         <div>
-            <NavLink to="/"> HOME </NavLink>
-            <NavLink to= "/selection"> SELCTION</NavLink>
-            <NavLink to="/sell"> SELL </NavLink>
+            <NavLink exact to="/" style={linkStyles} activeStyle={{background: "green"}}> HOME </NavLink>
+            <NavLink to= "/selection" style={linkStyles} activeStyle={{background: "green"}}> SELECTION</NavLink>
+            <NavLink to="/sell" style={linkStyles}  activeStyle={{background: "green"}}> SELL </NavLink>
         </div>
     )
 }
