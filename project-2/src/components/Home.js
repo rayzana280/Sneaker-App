ba972db116e1b9f136d89f90b6bb2736782787ff
Ray Zana <rayzana280@gmail.com>
2023-06-25 11:25:00 -0400

    adding classname for css

--- project-2/src/components/Home.js
+++ project-2/src/components/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function Home (){
     return (
-        <div>Home</div>
+        <div className="divHome">
+            <h1 className="textHome">Sneaker Up</h1>
+            <p className="textHome">Come and Buy The Newest Sneaker</p>
+            <p className="textHome">Sneaker Up Is Also A Place Where You Sell Shoes To Sneaker Head</p>
+        </div>
     )
 };
 
