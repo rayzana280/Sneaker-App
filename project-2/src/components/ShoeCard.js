9151ace2d47a597d4e58c9f3c4d301c814282f66
Ray Zana <rayzana280@gmail.com>
2023-06-25 00:08:34 -0400

    adding className to image to change size with css

--- project-2/src/components/ShoeCard.js
+++ project-2/src/components/ShoeCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function ShoeCard({name, color, image }){
+function ShoeCard({name, color, image, price }){
     return(
         <div>
             <h2>Name--{name}</h2>
+            <h3>Price--${price}</h3>
             <h5>ColorWay--{color}</h5>
-            <img src={image} alt="shoe image"/>
+            <img src={image} alt="shoe image" className="imageCard"/>
         </div>
     )
 }
