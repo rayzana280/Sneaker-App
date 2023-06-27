914e95cd6be39e7852c1931674a762893277b419
Ray Zana <rayzana280@gmail.com>
2023-06-27 16:23:32 -0400

    adding button for add cart and adding a click event which run a fetch request using post method updating the inCart value from false to true or true to false

--- project-2/src/components/ShoeCard.js
+++ project-2/src/components/ShoeCard.js
@@ -1,12 +1,26 @@
 import React from "react";
 
-function ShoeCard({name, color, image, price }){
+function ShoeCard({name, color, image, price, inCart, id , cartUpdate}){
+    function handleClick(){
+        fetch(`http://localhost:3000/shoe/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                inCart: !inCart,
+            }),
+        })
+        .then((r)=> r.json())
+        .then((updatedCart)=> cartUpdate(updatedCart))
+    }
     return(
         <div>
             <h2>Name--{name}</h2>
             <h3>Price--${price}</h3>
             <h5>ColorWay--{color}</h5>
             <img src={image} alt="shoe image" className="imageCard"/>
+            <button onClick={handleClick}>{inCart ? "Remove From" : "Add To"} Cart</button>
         </div>
     )
 }
