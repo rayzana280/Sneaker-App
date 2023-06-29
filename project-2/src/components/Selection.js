8708e76201a9aa8c94e489ac8c1a79335ed74fa0
Ray Zana <rayzana280@gmail.com>
2023-06-29 11:34:08 -0400

    adding more elemens and passsing a prop of callback to shoeCard component to get updated data

--- project-2/src/components/Selection.js
+++ project-2/src/components/Selection.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ShoeCard from "./ShoeCard" ;
 
-function Selection ({shoeData}){
+function Selection ({shoeData, cartUpdate}){
     //console.log(shoeData)
     return(
         <div>
-            <p>GET THE SNEAKERS FOR YOU</p>
+            <h1>GET THE SNEAKERS FOR YOU</h1>
             {shoeData.map((item)=>{
-                return <ShoeCard key={item.name} name={item.name} color={item.color} image={item.image}/>
+                return <ShoeCard key={item.id} name={item.name} color={item.color} image={item.image} price={item.price} inCart={item.inCart} id={item.id} cartUpdate={cartUpdate}/>
             })}
+            <p>This Everything We Have In Stock</p>
         </div>
     )
 };
