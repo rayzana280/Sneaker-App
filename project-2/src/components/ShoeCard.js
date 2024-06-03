9d90ad0861e4363ee5621691e9c12cc9fd4b6a42
Ray Zana <rayzana280@gmail.com>
2024-06-03 12:33:35 -0400

    since we are adding a back-end using ruby, then I had to change the Url for fetch, we using the method Patch this request would be changing the boolean for the item being in the cart or not

--- project-2/src/components/ShoeCard.js
+++ project-2/src/components/ShoeCard.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function ShoeCard({name, color, image, price, inCart, id , cartUpdate}){
+function ShoeCard({name, color, image, price, inCart, id , cartUpdate, shoeSize}){
     function handleClick(){
-        fetch(`http://localhost:3000/shoe/${id}`, {
+        fetch(`http://localhost:9292/shoes/${id}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
@@ -14,13 +14,18 @@ function ShoeCard({name, color, image, price, inCart, id , cartUpdate}){
         .then((r)=> r.json())
         .then((updatedCart)=> cartUpdate(updatedCart))
     }
+    
+    if(!image){
+        return <h2>Loading...</h2>
+      }
     return(
-        <div>
+        <div className="divCard">
             <h2>Name--{name}</h2>
             <h3>Price--${price}</h3>
             <h5>ColorWay--{color}</h5>
+            <h5>Size--{shoeSize}</h5>
             <img src={image} alt="shoe image" className="imageCard"/>
-            <button onClick={handleClick}>{inCart ? "Remove From" : "Add To"} Cart</button>
+            <button className="buttonCard" onClick={handleClick}>{inCart ? "Remove From" : "Add To"} Cart</button>
         </div>
     )
 }
